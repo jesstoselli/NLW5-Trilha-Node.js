26d1aa252afe643f424c21623ffd249fe52f7683
Jessyca Toselli <jfamedeiros@gmail.com>
2021-04-20 21:21:01 -0300

    Settings Controller implemented

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
-
-app.get("/", (req, res) => {
-  return res.json({
-    message: "Hello NLW#05",
-  });
-});
-
-app.post("/", (req, res) => {
-  return res.json({
-    message: "User successfully created.",
-  });
-});
 
 /*
 GET
